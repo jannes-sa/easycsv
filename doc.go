1ee55297286518441af00384a90af37f8ab5d298
jannes-sa <jannes.sa@tnis.com>
2017-11-10 15:34:52 +0700

    A

--- doc.go
+++
@@ -1,4 +0,0 @@
-// Package easycsv provides API to read CSV files with less boilerplate code in Go.
-//
-// Overview guideline: https://github.com/yunabe/easycsv/blob/master/README.md
-package easycsv
